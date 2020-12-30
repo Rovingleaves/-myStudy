4d2853de89c4186fa95df164978a260766ef4954
Rovingleaves <36840103+Rovingleaves@users.noreply.github.com>
2020-12-30 11:50:36 +0800

    Add files via upload

--- hello.py
+++ hello.py
@@ -8,9 +8,21 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
+from flask_mail import Message
+from threading import Thread
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['OPPYUBB_MAIL_SUBJECT_PREFIX'] = '[oppyubb] '
+app.config['OPPYUBB_MAIL_SENDER'] = 'oppyubb Admin<oppyubb@gmail.com>'
+app.config['OPPYUBB_ADMIN'] = os.environ.get('OPPYUBB_ADMIN')
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SECRET_KEY'] = 'rovin_flask'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -18,6 +30,7 @@ db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 class Role(db.Model):
     __tablename__= 'roles'
@@ -40,6 +53,19 @@ class User(db.Model):
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators = [DataRequired()])
     submit = SubmitField('Submit')
+    
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['OPPYUBB_MAIL_SUBJECT_PREFIX']+subject,\
+          sender=app.config['OPPYUBB_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template+ '.txt', **kwargs)
+    msg.html = render_template(template+ '.html',**kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -49,11 +75,14 @@ def index():
         if user is None:
             user = User(username=form.name.data)
             db.session.add(user)
-            db.session.commit()
+            #db.session.commit()
             session['known']=False
+            if app.config['OPPYUBB_ADMIN']:
+                send_email(app.config['OPPYUBB_ADMIN'],'New User','mail/new_user',user=user)
         else:
             session['known']=True
         session['name']=form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
     return render_template('index.html',
                            name = session.get('name'),
